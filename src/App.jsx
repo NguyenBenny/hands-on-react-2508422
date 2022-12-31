97c314680097acaf2b46d024c7c2aa485f4c21d0
NguyenBenny <79331970+NguyenBenny@users.noreply.github.com>
2022-12-31 10:25:13 -0800

    After exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import React, {Component} from "react";
+
+class Welcome extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return <h1>Meet the {this.props.name}</h1>;
+  }
+}
+
+const Welcome2 = (props) => {
+  return <h1>Meet the {props.name} 2</h1>
+}
+
 function App() {
   return (
     <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+      <Welcome2 name="Stars"/>
+      <p>
+        Members of an <b>intergalactic alliance</b> paving the way for peace and
+        benevolence among all species. They are known for their enthusiasm for
+        science, for their love of fun, and their dedication to education.
+      </p>
+      <button className="outline" onClick={() => alert("Clicked !")}>
+        Click me!
+      </button>
     </hgroup>
-  )
+  );
 }
-export default App
+export default App 
